3e6c7ecc9a5b1fae1aafd7a71b12b7430c708fc3
Maria <mpviegas23@gmail.com>
2019-04-05 19:51:59 +0100

    leitura de strings

--- TP3_TarefaA.java
+++ TP3_TarefaA.java
@@ -1,50 +1,52 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
 import java.text.NumberFormat;
 import java.util.*;
+import java.util.Scanner;
 
 class Movie{
+    public String name;
+    public int rents;
+    public int date;
+
+    Movie(String name, int rents, int date){
+        this.name = name;
+        this.rents = rents;
+        this.date = date;
+    }
 
 }
 
 public class TP3_TarefaA {
     public static void main (String[] arguments) {
-        String input, comando;
-        int posMovie;
-	    StringTokenizer st;
-        
-        do {  // enquanto houver mais linhas para ler...
-            input = readLn(200);
-            st= new StringTokenizer(input.trim());
-            comando = st.nextToken();
-            if (comando.equals("DATA")) {
-                posMovie = Integer.parseInt(st.nextToken());
-            } else if (comando.equals("POPULARIDADE")) {
-                posMovie = Integer.parseInt(st.nextToken());
-            } else if (comando.equals("NOME")) {
-                posMovie = Integer.parseInt(st.nextToken());
-            } else {
+        Scanner sc = new Scanner(System.in);
+        String movieQt = sc.nextLine();
+        ArrayList<Movie> dataBase = new ArrayList<Movie>();
+        ArrayList<Movie> oldToNew = new ArrayList<Movie>();
+        ArrayList<Movie> popular = new ArrayList<Movie>();
+        ArrayList<Movie> alphabetical = new ArrayList<Movie>();
 
-            }
-        } while (true);
+        dataBase = createDataBase(Integer.parseInt(movieQt));
 
     }
-    // leitura do input
-    static String readLn (int maxLg){ //utility function to read from stdin
-        byte lin[] = new byte [maxLg];
-        int lg = 0, car = -1;
-        //String line = "";
-        try {
-            while (lg < maxLg){
-                car = System.in.read();
-                if ((car < 0) || (car == '\n')) break;
-                lin [lg++] += car;
-            }
+
+    public static ArrayList<Movie>  createDataBase (int movieQt){ //utility function to read from stdin
+        ArrayList<Movie> movieList = new ArrayList<Movie>();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i < movieQt; i++){
+            String movieInfo = sc.nextLine();
+            //System.out.println(movieInfo);
+            String[] parts = movieInfo.split(" ");
+            int movieDate = Integer.parseInt(parts[0]);
+            //System.out.println(movieDate);
+            int movieRent = Integer.parseInt(parts[1]);
+            //System.out.println(movieRent);
+            String movieName = String.join(" ", Arrays.copyOfRange(parts, 2, parts.length));
+            //System.out.println(movieName);
+            Movie newMovie = new Movie(movieName, movieRent, movieDate);
+            movieList.add(newMovie);
         }
-        catch (IOException e){
-            return (null);
-        }
-        if ((car < 0) && (lg == 0)) return (null);  // eof
-        return (new String (lin, 0, lg));
+        return movieList;
     }
 } 
